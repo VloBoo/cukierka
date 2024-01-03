35330765d73039077005ea666e55cb67cef1fcf6
Uladzislau Charniakou <vlobo2004@gmail.com>
2024-01-03 04:51:59 +0300

    Добавлена в БД две новых таблички. В тестовом результате API возращается список полей таблиц

--- db/tables.sql
+++ db/tables.sql
@@ -4,3 +4,16 @@ CREATE TABLE Users(
     email VARCHAR NOT NULL, 
     other JSONB 
 );  
+
+CREATE TABLE Projects(
+    id UUID PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    applications VARCHAR[] NOT NULL,
+    other JSONB 
+);  
+
+CREATE TABLE UsersToProjects(
+    user_id UUID REFERENCES Users(id),
+    project_id UUID REFERENCES Projects(id),
+    user_role VARCHAR NOT NULL
+);  
--- src/main/java/by/vlobo/Database.java
+++ src/main/java/by/vlobo/Database.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Properties;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 // Этот класс должен служить прослойкой между бэком, что бы я спокойно вносил правки в бд и тут, а само приложение не трогал.
@@ -25,7 +26,7 @@ public class Database {
 
     public Database(String user, String password, String host, String dbName) {
         try {
-            String url = "jdbc:postgresql://" + host + "/"+dbName;
+            String url = "jdbc:postgresql://" + host + "/" + dbName;
             Properties props = new Properties();
             props.setProperty("user", user);
             props.setProperty("password", password);
@@ -39,7 +40,7 @@ public class Database {
         this(user, password, "localhost", "cukierka");
     }
 
-    public String basecheck() {
+    public String basechec2k() {
         try {
             Statement st = connection.createStatement();
             ResultSet rs = st.executeQuery("SELECT datname FROM pg_database;");
@@ -50,6 +51,42 @@ public class Database {
             rs.close();
             st.close();
             return new JSONObject().put("columns", columns.toArray()).toString();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String basecheck() {
+        try {
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery("SELECT \n" + //
+                    "    table_name as \"Таблица\",\n" + //
+                    "    column_name as \"Поле\"\n" + //
+                    "FROM \n" + //
+                    "    information_schema.columns\n" + //
+                    "WHERE \n" + //
+                    "    table_catalog = 'cukierka'\n" + //
+                    "    AND table_schema NOT LIKE 'pg_%'\n" + //
+                    "    AND table_schema != 'information_schema'\n" + //
+                    "ORDER BY \n" + //
+                    "    table_name, ordinal_position;\n" + //
+                    "");
+            JSONArray jsonArray = new JSONArray();
+
+            while (rs.next()) {
+                int columns = rs.getMetaData().getColumnCount();
+                JSONObject jsonObject = new JSONObject();
+
+                for (int i = 1; i <= columns; i++) {
+                    String columnName = rs.getMetaData().getColumnName(i);
+                    Object value = rs.getObject(i);
+                    jsonObject.put(columnName, value);
+                }
+
+                jsonArray.put(jsonObject);
+            }
+            return jsonArray.toString(4);
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
