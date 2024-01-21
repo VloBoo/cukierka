739fdb070a6c3718a72be25f4b7c5c653c3e4071
Uladzislau Charniakou <vlobo2004@gmail.com>
2024-01-22 02:20:03 +0300

    Сделана панель навигации. Добавлены другие вещи.

--- www/js/userbar.js
+++ www/js/userbar.js
@@ -1,15 +1,15 @@
 function userbar() {
     let token = getCookie("_t")
     if (token === undefined) {
-        document.getElementById("username").innerText = "Пользователь: Гость";
-        document.getElementById("logout").style.display = "none"
+        document.getElementById("username").innerText = "Привет Гость";
+        document.getElementById("logout").disabled = true;
 
     } else {
         send("GetUserInfo", token, {}).then((data) => {
             if (data.code == 200) {
-                document.getElementById("username").innerText = "Пользователь: " + data.username;
-                document.getElementById("signin").style.display = "none"
-                document.getElementById("signup").style.display = "none"
+                document.getElementById("username").innerText = "Привет " + data.username + "!";
+                document.getElementById("signin").disabled = true;
+                document.getElementById("signup").disabled = true;
             } else {
                 document.getElementById("username").innerText = "Ошибка";
             }
