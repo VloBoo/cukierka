a5e6a752bbaf6687a4d86a75df2cdd401ea0e2ab
Uladzislau Charniakou <vlobo2004@gmail.com>
2024-02-26 08:36:02 +0300

    Добавлена система логирования

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
-use uuid::uuid;
+use log::{error, info};
+use uuid::{uuid, Uuid};
 use warp::Filter;
+use log4rs;
 
 mod core;
 mod db;
-
+    
 #[tokio::main]
 async fn main() {
-
-    let get_tables_route = warp::path!("e").and_then(|| async {
-        let id = uuid!("78767867-e547-33e8-998b-3816d57e3e56");
-        match db::get_user(&id).await {
-            Ok(result) => {
-                eprintln!("{}", result);
-                return Ok::<_, warp::Rejection>(warp::reply::json(&result));
-            }
-            Err(err) => {
-                eprintln!("Error: {:?}", err);
-                return Ok::<_, warp::Rejection>(warp::reply::json(&"Error".to_string()));
-            }
-        };
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap_or_else(|e| {
+        eprintln!("Failed to initialize log4rs: {}", e);
+        std::process::exit(1);
     });
+
+    info!("Info test");
+    error!("Error test");
+    
+    let get_tables_route =
+        warp::path("e")
+            .and(warp::path::param())
+            .and_then(|param: String| async move {
+                match Uuid::parse_str(&param) {
+                    Ok(r) => match db::get_user(&r).await {
+                        Ok(result) => {
+                            info!("{}", result);
+                            return Ok::<_, warp::Rejection>(warp::reply::json(&result));
+                        }
+                        Err(err) => {
+                            error!("Error: {:?}", err);
+                            return Ok::<_, warp::Rejection>(warp::reply::json(
+                                &"Error".to_string(),
+                            ));
+                        }
+                    },
+                    Err(_) => {
+                        return Ok::<_, warp::Rejection>(warp::reply::json(&"Error".to_string()))
+                    }
+                }
+            });
 
     let _get_tables_route2 = warp::path!("w")
         .and_then(|| async { Ok::<_, warp::Rejection>(warp::reply::html("Hello".to_string())) });
