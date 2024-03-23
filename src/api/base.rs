fc7d5c62f48eb883fb8e5032002f387d15e84df9
Uladzislau Charniakou <vlobo2004@gmail.com>
2024-03-23 21:07:10 +0300

    Отложена идея с кэшированием. Реализовано минимальное количество систем для работы сайта для демонстрации

--- src/api/base.rs
+++ src/api/base.rs
@@ -1,21 +1,6 @@
-use serde_json::{json, Value};
+use serde_json::Value;
 use uuid::Uuid;
 
-use crate::db::DB;
-
-pub async fn get_user_by_id(uuid: &Uuid) -> Value {
-    log::debug!("get_user_by_id");
-    log::warn!("Небезопасная проверка создания бд");
-    let db = DB::new()
-        .await
-        .expect("Не удалось подключиться к базе данных");
-    log::debug!("exit new db");
-    let a = match db.get_user(uuid).await {
-        Ok(value) => value,
-        Err(_) => {
-            json!({"Error": "Error"})
-        }
-    };
-    log::trace!("'get_user_by_id' output:{:?}", a);
-    return a;
+pub trait Base {
+    fn get_user_by_id(self, uuid: &Uuid) -> Value;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,106 @@
+use serde_json::{json, Value};
+use std::{collections::HashMap, time::SystemTime, usize};
+use tokio_postgres::NoTls;
 use uuid::Uuid;
+use warp::http::StatusCode;
 use warp::Filter;
-
-mod api;
-mod db;
-
 #[tokio::main]
 async fn main() {
     pretty_env_logger::formatted_timed_builder()
-        .filter_level(log::LevelFilter::Trace)
+        .filter_level(log::LevelFilter::Debug)
         .init();
 
-    log::trace!("Trace test");
-    log::debug!("Debug test");
-    log::info!("Info test");
-    log::warn!("Warn test");
-    log::error!("Error test");
+    let sql_proxy = warp::path("sql").and(warp::body::json()).and_then(
+        |simple_map: HashMap<String, String>| async move {
+            let (client, connection) =
+                tokio_postgres::connect("host=localhost user=VloBo dbname=db", NoTls)
+                    .await
+                    .inspect_err(|e| {
+                        log::error!("Не удалось подключиться к базе данных \n {:?}", e)
+                    })
+                    .expect("Не удалось подключиться к базе данных");
+            tokio::spawn(async move {
+                if let Err(e) = connection.await {
+                    log::error!("connection error: {}", e);
+                }
+            });
+            let sql = match simple_map.get("sql") {
+                Some(sql) => sql,
+                None => {
+                    log::error!("Строка запроса не найдена \n {:?}", simple_map);
+                    panic!("Строка запроса не найдена!");
+                }
+            };
 
-    let get_tables_route = warp::path("api")
-        //.and(warp::path("e"))
-        .and(warp::path::param())
-        .and_then(|param: Uuid| async move {
-            log::debug!("test api");
-            Ok::<_, warp::Rejection>(warp::reply::json(&api::base::get_user_by_id(&param).await))
-        });
+            log::info!("{}", sql);
 
-    let test = warp::path("hi")
-        .and_then(|| async move { Ok::<_, warp::Rejection>(warp::reply::html("hello")) });
+            let rows = client
+                .query(sql, &[])
+                .await
+                .inspect_err(|e| log::error!("Не удалось отправить запрос \n {:?}", e))
+                .expect("Не удалось отправить запрос");
 
-    /*
-    let _hi = warp::path("q")
-        .and(warp::path::param())
-        .and(warp::header("user-agent"))
-        .map(|param: String, agent: String| format!("Hello {}, whose agent is {}", param, agent));
-    */
+            let mut rows_json = Vec::new();
 
-    warp::serve(get_tables_route.or(test))
-        .tls()
-        .cert_path("secret/cert.crt")
-        .key_path("secret/key.rsa")
-        .run(([0, 0, 0, 0], 443))
+            for row in &rows {
+                let mut row_json = serde_json::Map::new();
+
+                for (i, column) in row.columns().iter().enumerate() {
+                    let column_name = column.name();
+                    let value = if let Ok(value) = row.try_get::<usize, String>(i) {
+                        json!(value)
+                    } else if let Ok(value) = row.try_get::<usize, Uuid>(i) {
+                        json!(value.to_string())
+                    } else if let Ok(value) = row.try_get::<usize, SystemTime>(i) {
+                        json!(value)
+                    } else if let Ok(value) = row.try_get::<usize, Value>(i) {
+                        json!(value)
+                    } else {
+                        Value::Null
+                    };
+                    row_json.insert(column_name.into(), value);
+                }
+
+                rows_json.push(Value::Object(row_json));
+            }
+            let result = json!({
+                "rows": rows_json
+            });
+
+            Ok::<_, warp::Rejection>(warp::reply::json(&result))
+        },
+    );
+
+    let mail_sender = warp::path("email")
+        .and_then(|| async move { Ok::<_, warp::Rejection>(warp::reply::html("No")) });
+
+    let dir = warp::fs::dir("./www/");
+
+    let api_also = warp::any().and_then(|| async move {
+        Ok::<_, warp::Rejection>(warp::reply::json(&json!({"error":"404. Not Found."})))
+    });
+
+    let not_found = warp::any().map(|| {
+        warp::reply::with_status(
+            warp::reply::html("<h1 align=\"center\">404. Not Found.</h1>"),
+            StatusCode::NOT_FOUND,
+        )
+    });
+
+    let final_warp = warp::any()
+        .and(
+            warp::post().and(
+                warp::path("api")
+                    .and(sql_proxy.or(mail_sender))
+                    .or(api_also),
+            ),
+        )
+        .or(dir.or(not_found));
+
+    warp::serve(final_warp)
+        //.tls()
+        // .cert_path("secret/cert.crt")
+        // .key_path("secret/key.rsa")
+        .run(([0, 0, 0, 0], 80))
         .await;
 }
