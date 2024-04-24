59660bf1bd2e611445c0582786a6a1121c134fda
Uladzislau Charniakou <vlobo2004@gmail.com>
2024-04-25 02:38:57 +0300

    Заменен крейт с postgres, на slqx. Так же вынесена реализвация в отдельный файл

---
+++ backend/src/database.rs
@@ -0,0 +1,62 @@
+use chrono::Utc;
+use serde_json::{json, Value};
+use sqlx::postgres::PgPool;
+use sqlx::{Column, Pool, Postgres, Row};
+use std::error::Error;
+use uuid::Uuid;
+
+pub struct Database {
+    pool: Pool<Postgres>,
+}
+
+impl Database {
+    pub async fn new() -> Result<Database, Box<dyn Error>> {
+        let connection_str = "postgres://akreikuc:ackurkeiiekrukca@localhost:5432/cukierka";
+        let pool = PgPool::connect(connection_str).await?;
+        Ok(Database { pool })
+    }
+
+    pub async fn hardsql(sql: &str) -> Result<Value, Box<dyn Error>> {
+        let db = Database::new().await?;
+        let rows = sqlx::query(sql).fetch_all(&db.pool).await?;
+
+        let mut rows_json = Vec::new();
+
+        for row in &rows {
+            let mut row_json = serde_json::Map::new();
+
+            for (i, column) in row.columns().iter().enumerate() {
+                let column_name = column.name();
+                let value = if let Ok(value) = row.try_get::<String, usize>(i) {
+                    json!(value)
+                } else if let Ok(value) = row.try_get::<Uuid, usize>(i) {
+                    json!(value.to_string())
+                } else if let Ok(value) = row.try_get::<chrono::DateTime<Utc>, usize>(i) {
+                    json!(value.to_rfc3339())
+                } else if let Ok(value) = row.try_get::<chrono::NaiveDate, usize>(i) {
+                    json!(value.to_string())
+                } else if let Ok(value) = row.try_get::<i32, usize>(i) {
+                    json!(value)
+                } else if let Ok(value) = row.try_get::<i8, usize>(i) {
+                    json!(value)
+                } else if let Ok(value) = row.try_get::<i64, usize>(i) {
+                    json!(value)
+                } else if let Ok(value) = row.try_get::<Vec<String>, usize>(i) {
+                    json!(value)
+                } else if let Ok(value) = row.try_get::<Value, usize>(i) {
+                    json!(value)
+                } else {
+                    Value::Null
+                };
+                row_json.insert(column_name.into(), value);
+            }
+
+            rows_json.push(Value::Object(row_json));
+        }
+        let result = json!({
+            "rows": rows_json
+        });
+        
+        Ok(result)
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,10 @@
-use chrono::Utc;
-use serde_json::{json, Value};
-use std::{collections::HashMap, usize};
-use tokio_postgres::NoTls;
-use uuid::Uuid;
-use warp::http::StatusCode;
+use serde_json::json;
+use std::collections::HashMap;
 use warp::Filter;
+
+use crate::database::Database;
+
+mod database;
 
 #[tokio::main]
 async fn main() {
@@ -12,20 +12,10 @@ async fn main() {
         .filter_level(log::LevelFilter::Debug)
         .init();
 
+    log::info!("{:?}", Database::hardsql("SELECT 1 + 1 as sum").await);
+
     let sql_proxy = warp::path("sql").and(warp::body::json()).and_then(
         |simple_map: HashMap<String, String>| async move {
-            let (client, connection) =
-                tokio_postgres::connect("host=localhost user=VloBo dbname=db", NoTls)
-                    .await
-                    .inspect_err(|e| {
-                        log::error!("Не удалось подключиться к базе данных \n {:?}", e)
-                    })
-                    .expect("Не удалось подключиться к базе данных");
-            tokio::spawn(async move {
-                if let Err(e) = connection.await {
-                    log::error!("connection error: {}", e);
-                }
-            });
             let sql = match simple_map.get("sql") {
                 Some(sql) => sql,
                 None => {
@@ -36,85 +26,18 @@ async fn main() {
 
             log::info!("{}", sql);
 
-            let rows = match client.query(sql, &[]).await {
-                Ok(value) => {
-                    log::debug!("{:?}", value);
-                    value
-                }
-                Err(error) => {
-                    log::error!("Не удалось отправить запрос \n {:?}", error);
-                    return Ok::<_, warp::Rejection>(warp::reply::json(
-                        &json!({"error":"Not execute."}),
-                    ));
-                }
-            };
+            let result = Database::hardsql(sql).await.unwrap();
 
-            let mut rows_json = Vec::new();
-
-            for row in &rows {
-                let mut row_json = serde_json::Map::new();
-
-                for (i, column) in row.columns().iter().enumerate() {
-                    let column_name = column.name();
-                    let value = if let Ok(value) = row.try_get::<usize, String>(i) {
-                        json!(value)
-                    } else if let Ok(value) = row.try_get::<usize, Uuid>(i) {
-                        json!(value.to_string())
-                    } else if let Ok(value) = row.try_get::<usize, chrono::DateTime<Utc>>(i) {
-                        json!(value.to_rfc3339())
-                    } else if let Ok(value) = row.try_get::<usize, chrono::NaiveDate>(i) {
-                        json!(value.to_string())
-                    } else if let Ok(value) = row.try_get::<usize, i32>(i) {
-                        json!(value)
-                    } else if let Ok(value) = row.try_get::<usize, i8>(i) {
-                        json!(value)
-                    } else if let Ok(value) = row.try_get::<usize, i64>(i) {
-                        json!(value)
-                    } else if let Ok(value) = row.try_get::<usize, Vec<String>>(i) {
-                        json!(value)
-                    } else if let Ok(value) = row.try_get::<usize, Value>(i) {
-                        json!(value)
-                    } else {
-                        Value::Null
-                    };
-                    row_json.insert(column_name.into(), value);
-                }
-
-                rows_json.push(Value::Object(row_json));
-            }
-            let result = json!({
-                "rows": rows_json
-            });
-            log::debug!("{:?}", result);
             Ok::<_, warp::Rejection>(warp::reply::json(&result))
         },
     );
-
-    let mail_sender = warp::path("email")
-        .and_then(|| async move { Ok::<_, warp::Rejection>(warp::reply::html("No")) });
-
-    let dir = warp::fs::dir("./www/");
 
     let api_also = warp::any().and_then(|| async move {
         Ok::<_, warp::Rejection>(warp::reply::json(&json!({"error":"404. Not Found."})))
     });
 
-    let not_found = warp::any().map(|| {
-        warp::reply::with_status(
-            warp::reply::html("<h1 align=\"center\">404. Not Found.</h1>"),
-            StatusCode::NOT_FOUND,
-        )
-    });
-
-    let final_warp = warp::any()
-        .and(
-            warp::post().and(
-                warp::path("api")
-                    .and(sql_proxy.or(mail_sender))
-                    .or(api_also),
-            ),
-        )
-        .or(dir.or(not_found));
+    let final_warp =
+        warp::any().and(warp::post().and(warp::path("api").and(sql_proxy).or(api_also)));
 
     warp::serve(final_warp)
         //.tls()
