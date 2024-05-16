76158a15e04f20fa6b78ffbaee705eeedfe1141e
Uladzislau Charniakou <vlobo2004@gmail.com>
2024-05-16 22:17:39 +0300

    Чето добавил

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOMClient from 'react-dom/client';
+import Header from "./Header";
+import './css/tooplate.css';
+//import './css/fontawesome.css';
+import './css/bootstrap.css';
+
+const hello = 'Hello World';
 
 const elements = (
-  <div className='name'>
-    <h1>Hello</h1>
+  <div className="container">
+    <Header />
+    <h1>{hello}</h1>
   </div>
 )
 
@@ -11,6 +18,6 @@ const rootElement = document.getElementById("root");
 if (rootElement === null) {
   console.error("Не удалось найти корневой элемент");
 } else {
-  const root = ReactDOM.createRoot(rootElement);
+  const root = ReactDOMClient.createRoot(rootElement);
   root.render(elements);
 }
